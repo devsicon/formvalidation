8aebb79cbc6fcd0b2c70470e4d67060bbee46d5e
mmhasangit <ktumi.bolbona@gmail.com>
2021-06-21 13:26:05 +0600

    update js code

--- js/main.js
+++ js/main.js
@@ -1,15 +1,12 @@
 
 const inputs = document.querySelectorAll('input');
 
-//regex patterns
 const patterns = { 
-    fName: /^[a-z\d\s]{5,12}$/i,
-    lName: /^[a-z\d\s]{5,12}$/i,
+    fName: /^[a-z.-\s]{3,30}$/i,
+    lName: /^[a-z\d\s]{3,20}$/i,
     password: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W])[0-9a-zA-Z!@#$%^&*()+=-\?;,./{}|\":<>\[\]\\\' ~_-]{8,20}$/,
     eMail: /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/,
 }
-
-//validation function
 function validate(field, regex){
     if(regex.test(field.value)){
         field.className = 'valid form-control'
@@ -17,8 +14,6 @@ function validate(field, regex){
         field.className = 'invalid form-control'
     }
 }
-
-// attach keypu events to inputs
 inputs.forEach(input =>{
     input.addEventListener('keyup', e => {
         validate(e.target, patterns[e.target.attributes.name.value])
